862428f4aa4b9fc3711375e8dad0375621c8576c
Edwin Yu <edwinyyyu@gmail.com>
2023-03-20 02:01:42 -0700

    Don't load certain settings in VS Code

--- lua/eyu/settings.lua
+++ lua/eyu/settings.lua
@@ -1,21 +1,24 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+if not vim.g.vscode then
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
 
-vim.opt.termguicolors = true
+  vim.opt.termguicolors = true
 
-vim.opt.mouse = "a"
+  vim.opt.mouse = "a"
+
+  vim.opt.number = true
+
+  vim.opt.breakindent = true
+  vim.opt.linebreak = true
+
+  vim.opt.colorcolumn = "80"
+
+  vim.opt.cursorline = true
+  vim.opt.cursorcolumn = true
+
+  vim.opt.scrolloff = 8
+
+  vim.opt.pumheight = 8
+end
+
 vim.opt.clipboard = "unnamedplus"
-
-vim.opt.number = true
-
-vim.opt.breakindent = true
-vim.opt.linebreak = true
-
-vim.opt.colorcolumn = "80"
-
-vim.opt.cursorline = true
-vim.opt.cursorcolumn = true
-
-vim.opt.scrolloff = 8
-
-vim.opt.pumheight = 8
